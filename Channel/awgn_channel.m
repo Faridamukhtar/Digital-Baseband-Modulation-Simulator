86f2d3b766676dd96f8c14494c592a423c8b3938
chiz8622 <128740994+chiz8622@users.noreply.github.com>
2025-05-14 02:53:14 +0300

    added avg energy

--- Channel/awgn_channel.m
+++ Channel/awgn_channel.m
@@ -1,6 +1,7 @@
 function noisy = awgn_channel(signal, EbNo, k)
 % Add AWGN noise to baseband signal
     N0 = 1 / (10^(EbNo/10)); % assuming Eb = 1
-    noise = sqrt(N0 / (2*k)) * (randn(size(signal)) + 1j*randn(size(signal)));
+    Eavg = mean(abs(signal).^2);
+    noise = sqrt(N0 *Eavg/ (2*k)) * (randn(size(signal)) + 1j*randn(size(signal)));
     noisy = signal + noise;
 end
