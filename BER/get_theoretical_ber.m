9c3a111bbadc59f6a46515b7fa37f2a237c8b257
chiz8622 <128740994+chiz8622@users.noreply.github.com>
2025-05-14 02:56:35 +0300

    added MASK and MQAM

--- BER/get_theoretical_ber.m
+++ BER/get_theoretical_ber.m
@@ -4,7 +4,15 @@ function ber = get_theoretical_ber(EbNoVec, modType, M)
     switch lower(modType)
         case {'dbpsk'}
             ber = 0.5 * exp(-EbNo);
-
+        case {'mask'}  % M-ASK
+            k = log2(M);
+            factor = sqrt((3 * k * EbNo) ./ (M^2 - 1));
+            Ps = 2 * (1 - 1/M) .* 0.5 .* erfc(factor / sqrt(2));
+            ber = Ps / k;
+        case {'mqam'}  % Square M-QAM
+            k = log2(M);                   % Bits per symbol
+            factor = sqrt(3 * k ./ (M - 1) .* EbNo);
+            ber = (4 / k) * (1 - 1/sqrt(M)) .* 0.5 .* erfc(factor / sqrt(2));
    
         otherwise
             ber = 0;
